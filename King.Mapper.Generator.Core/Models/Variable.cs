33af9f0f8a284e648cc1369678a8d48ba998510d
Jef King <jamieking80@gmail.com>
2014-06-22 13:47:43 -0700

    class

---
+++ King.Mapper.Generator.Core/Models/Variable.cs
@@ -0,0 +1,23 @@
+﻿namespace King.Mapper.Generator.Models
+{
+    public class Variable
+    {
+        #region Properties
+        public string DataType
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int MaxLength
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
---
+++ King.Mapper.Generator.Unit/Models/VariableTests.cs
@@ -0,0 +1,50 @@
+﻿namespace King.Mapper.Generator.Unit.Models
+{
+    using King.Mapper.Generator.Models;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class VariableTest
+    {
+        #region Valid Cases
+        [TestMethod]
+        public void Constructor()
+        {
+            new Variable();
+        }
+
+        [TestMethod]
+        public void DataType()
+        {
+            var item = new Variable();
+            var data = Guid.NewGuid().ToString();
+            item.DataType = data;
+            Assert.AreEqual<string>(data, item.DataType);
+        }
+
+        [TestMethod]
+        public void Name()
+        {
+            var item = new Variable();
+            var data = Guid.NewGuid().ToString();
+            item.Name = data;
+            Assert.AreEqual<string>(data, item.Name);
+        }
+
+        [TestMethod]
+        public void MaxLength()
+        {
+            var random = new Random();
+            var item = new Variable();
+            var data = random.Next();
+            item.MaxLength = data;
+            Assert.AreEqual<int>(data, item.MaxLength);
+        }
+        #endregion
+    }
+}
