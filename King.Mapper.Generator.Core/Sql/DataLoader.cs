1d9e8de73ae2e2f36ab3941c0ccd3a8d9622efe0
Jef King <jamieking80@gmail.com>
2014-06-22 15:05:23 -0700

    Data LOader

---
+++ King.Mapper.Generator.Core/Sql/DataLoader.cs
@@ -0,0 +1,65 @@
+﻿namespace King.Mapper.Generator.Sql
+{
+    using King.Mapper.Data;
+    using King.Mapper.Generator.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Data Loader
+    /// </summary>
+    public class DataLoader : IDataLoader
+    {
+        #region Members
+        /// <summary>
+        /// Connection String
+        /// </summary>
+        private readonly string connectionString = null;
+
+        /// <summary>
+        /// Data Loader
+        /// </summary>
+        private readonly ILoader<Schema> loader = null;
+        #endregion
+
+        #region Constructors
+        public DataLoader(string connectionString, ILoader<Schema> loader)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("connectionString");
+            }
+            if (null == loader)
+            {
+                throw new ArgumentNullException("loader");
+            }
+
+            this.loader = loader;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Load data from Database, and return the models.
+        /// </summary>
+        /// <returns>Schemas to process</returns>
+        public async Task<IEnumerable<Schema>> Load()
+        {
+            IEnumerable<Schema> schemas = null;
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var execute = new SqlCommand(Statement.sqlStatement, connection))
+                {
+                    await connection.OpenAsync();
+
+                    schemas = loader.Models(execute);
+                }
+            }
+
+            return schemas;
+        }
+        #endregion
+    }
+}
---
+++ King.Mapper.Generator.Unit/Sql/DataLoaderTests.cs
@@ -0,0 +1,35 @@
+﻿namespace King.Mapper.Generator.Unit.Sql
+{
+    using King.Mapper.Data;
+    using King.Mapper.Generator.Models;
+    using King.Mapper.Generator.Sql;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NSubstitute;
+    using System;
+
+    [TestClass]
+    public class DataLoaderTests
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            var loader = Substitute.For<ILoader<Schema>>();
+            new DataLoader(Guid.NewGuid().ToString(), loader);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorConnectionStringNull()
+        {
+            var loader = Substitute.For<ILoader<Schema>>();
+            new DataLoader(null, loader);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorLoaderNull()
+        {
+            new DataLoader(Guid.NewGuid().ToString(), null);
+        }
+    }
+}
