223c9512cf086bc758a817031238e7a20eba0dad
Jef King <jamieking80@gmail.com>
2014-06-22 13:51:03 -0700

    Classes

---
+++ King.Mapper.Generator.Core/Models/Definition.cs
@@ -0,0 +1,50 @@
+﻿namespace King.Mapper.Generator.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Definition : IComparable
+    {
+        #region Properties
+        /// <summary>
+        /// Preface
+        /// </summary>
+        public string Preface
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Variables
+        /// </summary>
+        public IEnumerable<Variable> Variables
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Compare To
+        /// </summary>
+        /// <param name="other">Other</param>
+        /// <returns>Comparison</returns>
+        public int CompareTo(object other)
+        {
+            var otherProcInfo = other as Definition;
+            return this.Preface.CompareTo(otherProcInfo.Preface) + this.Name.CompareTo(otherProcInfo.Name);
+        }
+        #endregion
+    }
+}
---
+++ King.Mapper.Generator.Unit/Models/DefinitionTests.cs
@@ -0,0 +1,66 @@
+﻿namespace King.Mapper.Generator.Unit.Models
+{
+    using King.Mapper.Generator.Models;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+
+    [TestClass]
+    public class DefinitionTests
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            new Definition();
+        }
+
+        [TestMethod]
+        public void IsIComparable()
+        {
+            Assert.IsNotNull(new Definition() as IComparable);
+        }
+
+        [TestMethod]
+        public void Preface()
+        {
+            var item = new Definition();
+            var data = Guid.NewGuid().ToString();
+            item.Preface = data;
+            Assert.AreEqual<string>(data, item.Preface);
+        }
+
+        [TestMethod]
+        public void Name()
+        {
+            var item = new Definition();
+            var data = Guid.NewGuid().ToString();
+            item.Name = data;
+            Assert.AreEqual<string>(data, item.Name);
+        }
+
+        [TestMethod]
+        public void Variables()
+        {
+            var item = new Definition();
+            var data = new List<Variable>();
+            item.Variables = data;
+            Assert.AreEqual<IEnumerable<Variable>>(data, item.Variables);
+        }
+
+        [TestMethod]
+        public void CompareTo()
+        {
+            var item = new Definition()
+            {
+                Name = Guid.NewGuid().ToString(),
+                Preface = Guid.NewGuid().ToString(),
+            };
+            var compare = new Definition()
+            {
+                Name = Guid.NewGuid().ToString(),
+                Preface = Guid.NewGuid().ToString(),
+            };
+            Assert.AreEqual<int>(item.Preface.CompareTo(compare.Preface) + item.Name.CompareTo(compare.Name), item.CompareTo(compare));
+        }
+    }
+}
