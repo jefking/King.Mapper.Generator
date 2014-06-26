b17b0b9532127340960600d4da5f0821763b680d
Jef King <jamieking80@gmail.com>
2014-06-25 22:31:21 -0700

    test classes

---
+++ King.Mapper.Generator.Unit/Templates/StoredProceduresTests.cs
@@ -0,0 +1,14 @@
+﻿namespace King.Mapper.Generator.Unit.Templates
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class StoredProceduresTests
+    {
+    }
+}
