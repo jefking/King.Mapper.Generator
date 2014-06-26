47199eb1cf6c662510ad38e850d44abfae08ea82
Jef King <jamieking80@gmail.com>
2014-06-25 20:16:06 -0700

    integration tests

---
+++ King.Mapper.Generator.Integration/Integration.cs
@@ -0,0 +1,14 @@
+﻿namespace King.Mapper.Generator.Integration
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Integration
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ King.Mapper.Generator.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Mapper.Generator.Integration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jef King")]
+[assembly: AssemblyProduct("King.Mapper.Generator.Integration")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("72471475-143c-4319-81f5-05482e636da4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
