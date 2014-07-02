ec86d909c052fa271ff786e3c4fb042c7f37123c
Jef King <jamieking80@gmail.com>
2014-07-01 21:44:38 -0700

    test cases pass

---
+++ King.Mapper.Generator.Core/StoredProceduresTestsRender .cs
@@ -0,0 +1,22 @@
+﻿namespace King.Mapper.Generator
+{
+    using King.Mapper.Generator.Models;
+    using King.Mapper.Generator.Templates;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Stored Procedures Render
+    /// </summary>
+    public class StoredProceduresTestsRender : Renderer
+    {
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public StoredProceduresTestsRender(IDictionary<int, Definition> manifest)
+            : base(new StoredProceduresTests(), manifest, "StoredProcedures.Unit.Test.Generated.cs")
+        {
+        }
+        #endregion
+    }
+}
