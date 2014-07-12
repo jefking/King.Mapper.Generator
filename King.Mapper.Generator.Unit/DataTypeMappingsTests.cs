fa29e895e92dda3e903d1db2c795a278c3ffdbb4
Jef King <jamieking80@gmail.com>
2014-07-12 16:02:57 -0700

    Simplified test cases

--- King.Mapper.Generator.Unit/DataTypeMappingsTests.cs
+++ King.Mapper.Generator.Unit/DataTypeMappingsTests.cs
@@ -2,10 +2,6 @@
 {
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class DataTypeMappingsTests
@@ -16,90 +12,30 @@
         {
             DataTypeMappings.DataTypeCSharp(null);
         }
-
-        [Test]
-        public void DataTypeCSharpString()
+        
+        [TestCase("string", "varchar")]
+        [TestCase("string", "nvarchar")]
+        [TestCase("string", "ntext")]
+        [TestCase("string", "text")]
+        [TestCase("string", "string")]
+        [TestCase("byte[]", "binary")]
+        [TestCase("byte[]", "image")]
+        [TestCase("char", "char")]
+        [TestCase("char", "nchar")]
+        [TestCase("float?", "float")]
+        [TestCase("int?", "int")]
+        [TestCase("long?", "bigint")]
+        [TestCase("byte?", "tinyint")]
+        [TestCase("short?", "smallint")]
+        [TestCase("decimal?", "money")]
+        [TestCase("decimal?", "decimal")]
+        [TestCase("bool?", "bit")]
+        [TestCase("DateTime?", "datetime")]
+        [TestCase("Guid?", "uniqueidentifier")]
+        [TestCase("object", "HAHAH,BLAHBLAH,nothinghere")]
+        public void DataTypeCSharp(string expected, string value)
         {
-            Assert.AreEqual("string", DataTypeMappings.DataTypeCSharp("varchar"));
-            Assert.AreEqual("string", DataTypeMappings.DataTypeCSharp("nvarchar"));
-            Assert.AreEqual("string", DataTypeMappings.DataTypeCSharp("ntext"));
-            Assert.AreEqual("string", DataTypeMappings.DataTypeCSharp("text"));
-            Assert.AreEqual("string", DataTypeMappings.DataTypeCSharp("string"));
-        }
-
-        [Test]
-        public void DataTypeCSharpByteArray()
-        {
-            Assert.AreEqual("byte[]", DataTypeMappings.DataTypeCSharp("binary"));
-            Assert.AreEqual("byte[]", DataTypeMappings.DataTypeCSharp("image"));
-        }
-
-        [Test]
-        public void DataTypeCSharpChar()
-        {
-            Assert.AreEqual("char", DataTypeMappings.DataTypeCSharp("char"));
-            Assert.AreEqual("char", DataTypeMappings.DataTypeCSharp("nchar"));
-        }
-
-        [Test]
-        public void DataTypeCSharpFloat()
-        {
-            Assert.AreEqual("float?", DataTypeMappings.DataTypeCSharp("float"));
-        }
-
-        [Test]
-        public void DataTypeCSharpInt()
-        {
-            Assert.AreEqual("int?", DataTypeMappings.DataTypeCSharp("int"));
-        }
-
-        [Test]
-        public void DataTypeCSharpLong()
-        {
-            Assert.AreEqual("long?", DataTypeMappings.DataTypeCSharp("bigint"));
-        }
-
-        [Test]
-        public void DataTypeCSharpByte()
-        {
-            Assert.AreEqual("byte?", DataTypeMappings.DataTypeCSharp("tinyint"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShort()
-        {
-            Assert.AreEqual("short?", DataTypeMappings.DataTypeCSharp("smallint"));
-        }
-
-        [Test]
-        public void DataTypeCSharpDecimal()
-        {
-            Assert.AreEqual("decimal?", DataTypeMappings.DataTypeCSharp("money"));
-            Assert.AreEqual("decimal?", DataTypeMappings.DataTypeCSharp("decimal"));
-        }
-
-        [Test]
-        public void DataTypeCSharpBool()
-        {
-            Assert.AreEqual("bool?", DataTypeMappings.DataTypeCSharp("bit"));
-        }
-
-        [Test]
-        public void DataTypeCSharpDateTime()
-        {
-            Assert.AreEqual("DateTime?", DataTypeMappings.DataTypeCSharp("datetime"));
-        }
-
-        [Test]
-        public void DataTypeCSharpGuid()
-        {
-            Assert.AreEqual("Guid?", DataTypeMappings.DataTypeCSharp("uniqueidentifier"));
-        }
-
-        [Test]
-        public void DataTypeCSharp()
-        {
-            Assert.AreEqual("object", DataTypeMappings.DataTypeCSharp(Guid.NewGuid().ToString()));
+            Assert.AreEqual(expected, DataTypeMappings.DataTypeCSharp(value));
         }
 
         [Test]
@@ -108,101 +44,30 @@
         {
             DataTypeMappings.DataTypeDbType(null);
         }
-
-        [Test]
-        public void DataTypeDbTypeString()
+        [TestCase("DbType.String", "varchar")]
+        [TestCase("DbType.String", "nvarchar")]
+        [TestCase("DbType.String", "ntext")]
+        [TestCase("DbType.String", "text")]
+        [TestCase("DbType.String", "nchar")]
+        [TestCase("DbType.String", "string")]
+        [TestCase("DbType.DateTime2", "datetime2")]
+        [TestCase("DbType.Date", "date")]
+        [TestCase("DbType.Single", "float")]
+        [TestCase("DbType.Binary", "binary")]
+        [TestCase("DbType.Binary", "image")]
+        [TestCase("DbType.Int32", "int")]
+        [TestCase("DbType.DateTime", "datetime")]
+        [TestCase("DbType.Int64", "bigint")]
+        [TestCase("DbType.Byte", "tinyint")]
+        [TestCase("DbType.Int16", "smallint")]
+        [TestCase("DbType.Currency", "money")]
+        [TestCase("DbType.Decimal", "decimal")]
+        [TestCase("DbType.Boolean", "bit")]
+        [TestCase("DbType.Guid", "uniqueidentifier")]
+        [TestCase("DbType.Object", "I don't know what this is")]
+        public void DataTypeDbType(string expected, string value)
         {
-            Assert.AreEqual("DbType.String", DataTypeMappings.DataTypeDbType("varchar"));
-            Assert.AreEqual("DbType.String", DataTypeMappings.DataTypeDbType("nvarchar"));
-            Assert.AreEqual("DbType.String", DataTypeMappings.DataTypeDbType("ntext"));
-            Assert.AreEqual("DbType.String", DataTypeMappings.DataTypeDbType("text"));
-            Assert.AreEqual("DbType.String", DataTypeMappings.DataTypeDbType("nchar"));
-            Assert.AreEqual("DbType.String", DataTypeMappings.DataTypeDbType("string"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeDateTime2()
-        {
-            Assert.AreEqual("DbType.DateTime2", DataTypeMappings.DataTypeDbType("datetime2"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeDate()
-        {
-            Assert.AreEqual("DbType.Date", DataTypeMappings.DataTypeDbType("date"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeSingle()
-        {
-            Assert.AreEqual("DbType.Single", DataTypeMappings.DataTypeDbType("float"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeBinary()
-        {
-            Assert.AreEqual("DbType.Binary", DataTypeMappings.DataTypeDbType("binary"));
-            Assert.AreEqual("DbType.Binary", DataTypeMappings.DataTypeDbType("image"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeInt()
-        {
-            Assert.AreEqual("DbType.Int32", DataTypeMappings.DataTypeDbType("int"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeDateTime()
-        {
-            Assert.AreEqual("DbType.DateTime", DataTypeMappings.DataTypeDbType("datetime"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeBigInt()
-        {
-            Assert.AreEqual("DbType.Int64", DataTypeMappings.DataTypeDbType("bigint"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeTinyInt()
-        {
-            Assert.AreEqual("DbType.Byte", DataTypeMappings.DataTypeDbType("tinyint"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeSmallInt()
-        {
-            Assert.AreEqual("DbType.Int16", DataTypeMappings.DataTypeDbType("smallint"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeMoney()
-        {
-            Assert.AreEqual("DbType.Currency", DataTypeMappings.DataTypeDbType("money"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeDecimal()
-        {
-            Assert.AreEqual("DbType.Decimal", DataTypeMappings.DataTypeDbType("decimal"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeBit()
-        {
-            Assert.AreEqual("DbType.Boolean", DataTypeMappings.DataTypeDbType("bit"));
-        }
-
-        [Test]
-        public void DataTypeDbTypeGuid()
-        {
-            Assert.AreEqual("DbType.Guid", DataTypeMappings.DataTypeDbType("uniqueidentifier"));
-        }
-
-        [Test]
-        public void DataTypeDbType()
-        {
-            Assert.AreEqual("DbType.Object", DataTypeMappings.DataTypeDbType(Guid.NewGuid().ToString()));
+            Assert.AreEqual(expected, DataTypeMappings.DataTypeDbType(value));
         }
 
         [Test]
@@ -212,80 +77,24 @@
             DataTypeMappings.DataTypeCSharpShorten(null);
         }
 
-        [Test]
-        public void DataTypeCSharpShortenString()
+        [TestCase("string", "System.String")]
+        [TestCase("double", "system.double")]
+        [TestCase("int", "System.Int32")]
+        [TestCase("long", "System.Int64")]
+        [TestCase("byte", "System.Byte")]
+        [TestCase("short", "System.Int16")]
+        [TestCase("decimal", "System.Decimal")]
+        [TestCase("bool", "System.Boolean")]
+        [TestCase("DateTime", "System.DateTime")]
+        [TestCase("DateTime?", "System.Nullable`1[System.DateTime]")]
+        [TestCase("Guid", "System.Guid")]
+        [TestCase("Guid?", "system.nullable`1[system.guid]")]
+        [TestCase("Guid?", "System.Nullable`1[System.Guid]")]
+        [TestCase("object", "System.WhatIsThis")]
+        public void DataTypeCSharpShorten(string expected, string value)
         {
-            Assert.AreEqual("string", DataTypeMappings.DataTypeCSharpShorten("System.String"));
+            Assert.AreEqual(expected, DataTypeMappings.DataTypeCSharpShorten(value));
         }
-
-        [Test]
-        public void DataTypeCSharpShortenDouble()
-        {
-            Assert.AreEqual("double", DataTypeMappings.DataTypeCSharpShorten("system.double"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShortenInt32()
-        {
-            Assert.AreEqual("int", DataTypeMappings.DataTypeCSharpShorten("System.Int32"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShortenInt64()
-        {
-            Assert.AreEqual("long", DataTypeMappings.DataTypeCSharpShorten("System.Int64"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShortenByte()
-        {
-            Assert.AreEqual("byte", DataTypeMappings.DataTypeCSharpShorten("System.Byte"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShortenInt16()
-        {
-            Assert.AreEqual("short", DataTypeMappings.DataTypeCSharpShorten("System.Int16"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShortenDecimal()
-        {
-            Assert.AreEqual("decimal", DataTypeMappings.DataTypeCSharpShorten("System.Decimal"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShortenBool()
-        {
-            Assert.AreEqual("bool", DataTypeMappings.DataTypeCSharpShorten("System.Boolean"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShortenDateTime()
-        {
-            Assert.AreEqual("DateTime", DataTypeMappings.DataTypeCSharpShorten("System.DateTime"));
-            Assert.AreEqual("DateTime?", DataTypeMappings.DataTypeCSharpShorten("System.Nullable`1[System.DateTime]"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShortenGuid()
-        {
-            Assert.AreEqual("Guid", DataTypeMappings.DataTypeCSharpShorten("System.Guid"));
-            Assert.AreEqual("Guid?", DataTypeMappings.DataTypeCSharpShorten("system.nullable`1[system.guid]"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShortenNullableGuid()
-        {
-            Assert.AreEqual("Guid?", DataTypeMappings.DataTypeCSharpShorten("System.Nullable`1[System.Guid]"));
-        }
-
-        [Test]
-        public void DataTypeCSharpShorten()
-        {
-            Assert.AreEqual("object", DataTypeMappings.DataTypeCSharpShorten(Guid.NewGuid().ToString()));
-        }
-
 
         [Test]
         [ExpectedException(typeof(ArgumentException))]
@@ -294,107 +103,42 @@
             DataTypeMappings.DataTypeDbType(null);
         }
 
-        [Test]
-        public void DataTypeInitializeString()
+        [TestCase("Guid.NewGuid().ToString()", "string")]
+        [TestCase("Guid.NewGuid().ToString()", "System.String")]
+        [TestCase("(char)new Random().Next()", "char")]
+        [TestCase("(char)new Random().Next()", "System.Char")]
+        [TestCase("(char)new Random().Next()", "char?")]
+        [TestCase("(float)new Random().Next()", "float")]
+        [TestCase("(float)new Random().Next()", "System.Single")]
+        [TestCase("(float)new Random().Next()", "float?")]
+        [TestCase("new byte[128]; new Random().NextBytes(expected)", "byte[]")]
+        [TestCase("new Random().Next()", "System.Int32")]
+        [TestCase("new Random().Next()", "int?")]
+        [TestCase("new Random().Next()", "int")]
+        [TestCase("(long)new Random().Next()", "System.Int64")]
+        [TestCase("(long)new Random().Next()", "long?")]
+        [TestCase("(long)new Random().Next()", "long")]
+        [TestCase("(decimal)new Random().NextDouble()", "decimal?")]
+        [TestCase("(decimal)new Random().NextDouble()", "decimal")]
+        [TestCase("(decimal)new Random().NextDouble()", "System.Decimal")]
+        [TestCase("(byte)new Random().Next(byte.MinValue, byte.MaxValue)", "byte")]
+        [TestCase("(byte)new Random().Next(byte.MinValue, byte.MaxValue)", "byte?")]
+        [TestCase("(short)new Random().Next(short.MinValue, short.MaxValue)", "short")]
+        [TestCase("(short)new Random().Next(short.MinValue, short.MaxValue)", "short?")]
+        [TestCase("DateTime.UtcNow", "DateTime?")]
+        [TestCase("DateTime.UtcNow", "DateTime")]
+        [TestCase("DateTime.UtcNow", "System.Nullable`1[System.DateTime]")]
+        [TestCase("true", "bool?")]
+        [TestCase("true", "bool")]
+        [TestCase("Guid.NewGuid()", "Guid?")]
+        [TestCase("Guid.NewGuid()", "Guid")]
+        [TestCase("Guid.NewGuid()", "System.Nullable`1[System.Guid]")]
+        [TestCase("Guid.NewGuid()", "System.Guid")]
+        [TestCase("new object()", "object")]
+        [TestCase("null", "this is huh?")]
+        public void DataTypeInitialize(string expected, string value)
         {
-            Assert.AreEqual("Guid.NewGuid().ToString()", DataTypeMappings.DataTypeInitialize("string"));
-            Assert.AreEqual("Guid.NewGuid().ToString()", DataTypeMappings.DataTypeInitialize("System.String"));
-        }
-
-        [Test]
-        public void DataTypeInitializeChar()
-        {
-            Assert.AreEqual("(char)new Random().Next()", DataTypeMappings.DataTypeInitialize("char"));
-            Assert.AreEqual("(char)new Random().Next()", DataTypeMappings.DataTypeInitialize("System.Char"));
-            Assert.AreEqual("(char)new Random().Next()", DataTypeMappings.DataTypeInitialize("char?"));
-        }
-
-        [Test]
-        public void DataTypeInitializeFloat()
-        {
-            Assert.AreEqual("(float)new Random().Next()", DataTypeMappings.DataTypeInitialize("float"));
-            Assert.AreEqual("(float)new Random().Next()", DataTypeMappings.DataTypeInitialize("System.Single"));
-            Assert.AreEqual("(float)new Random().Next()", DataTypeMappings.DataTypeInitialize("float?"));
-        }
-
-        [Test]
-        public void DataTypeInitializeByteArray()
-        {
-            Assert.AreEqual("new byte[128]; new Random().NextBytes(expected)", DataTypeMappings.DataTypeInitialize("byte[]"));
-        }
-
-        [Test]
-        public void DataTypeInitializeInt()
-        {
-            Assert.AreEqual("new Random().Next()", DataTypeMappings.DataTypeInitialize("System.Int32"));
-            Assert.AreEqual("new Random().Next()", DataTypeMappings.DataTypeInitialize("int?"));
-            Assert.AreEqual("new Random().Next()", DataTypeMappings.DataTypeInitialize("int"));
-        }
-
-        [Test]
-        public void DataTypeInitializeLong()
-        {
-            Assert.AreEqual("(long)new Random().Next()", DataTypeMappings.DataTypeInitialize("System.Int64"));
-            Assert.AreEqual("(long)new Random().Next()", DataTypeMappings.DataTypeInitialize("long?"));
-            Assert.AreEqual("(long)new Random().Next()", DataTypeMappings.DataTypeInitialize("long"));
-        }
-
-        [Test]
-        public void DataTypeInitializeDecimal()
-        {
-            Assert.AreEqual("(decimal)new Random().NextDouble()", DataTypeMappings.DataTypeInitialize("decimal?"));
-            Assert.AreEqual("(decimal)new Random().NextDouble()", DataTypeMappings.DataTypeInitialize("decimal"));
-            Assert.AreEqual("(decimal)new Random().NextDouble()", DataTypeMappings.DataTypeInitialize("System.Decimal"));
-        }
-
-        [Test]
-        public void DataTypeInitializeByte()
-        {
-            Assert.AreEqual("(byte)new Random().Next(byte.MinValue, byte.MaxValue)", DataTypeMappings.DataTypeInitialize("byte"));
-            Assert.AreEqual("(byte)new Random().Next(byte.MinValue, byte.MaxValue)", DataTypeMappings.DataTypeInitialize("byte?"));
-        }
-
-        [Test]
-        public void DataTypeInitializeShort()
-        {
-            Assert.AreEqual("(short)new Random().Next(short.MinValue, short.MaxValue)", DataTypeMappings.DataTypeInitialize("short"));
-            Assert.AreEqual("(short)new Random().Next(short.MinValue, short.MaxValue)", DataTypeMappings.DataTypeInitialize("short?"));
-        }
-
-        [Test]
-        public void DataTypeInitializeDateTime()
-        {
-            Assert.AreEqual("DateTime.UtcNow", DataTypeMappings.DataTypeInitialize("DateTime?"));
-            Assert.AreEqual("DateTime.UtcNow", DataTypeMappings.DataTypeInitialize("DateTime"));
-            Assert.AreEqual("DateTime.UtcNow", DataTypeMappings.DataTypeInitialize("System.Nullable`1[System.DateTime]"));
-        }
-
-        [Test]
-        public void DataTypeInitializeBool()
-        {
-            Assert.AreEqual("true", DataTypeMappings.DataTypeInitialize("bool?"));
-            Assert.AreEqual("true", DataTypeMappings.DataTypeInitialize("bool"));
-        }
-
-        [Test]
-        public void DataTypeInitializeGuid()
-        {
-            Assert.AreEqual("Guid.NewGuid()", DataTypeMappings.DataTypeInitialize("Guid?"));
-            Assert.AreEqual("Guid.NewGuid()", DataTypeMappings.DataTypeInitialize("Guid"));
-            Assert.AreEqual("Guid.NewGuid()", DataTypeMappings.DataTypeInitialize("System.Nullable`1[System.Guid]"));
-            Assert.AreEqual("Guid.NewGuid()", DataTypeMappings.DataTypeInitialize("System.Guid"));
-        }
-
-        [Test]
-        public void DataTypeInitializeObject()
-        {
-            Assert.AreEqual("new object()", DataTypeMappings.DataTypeInitialize("object"));
-        }
-
-        [Test]
-        public void DataTypeInitialize()
-        {
-            Assert.AreEqual("null", DataTypeMappings.DataTypeInitialize(Guid.NewGuid().ToString()));
+            Assert.AreEqual(expected, DataTypeMappings.DataTypeInitialize(value));
         }
     }
 }
