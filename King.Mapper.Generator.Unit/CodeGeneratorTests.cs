0dc8e6b9a1002903a753dbe4b4d6951585328755
Jef King <jefkin@microsoft.com>
2018-02-25 10:50:36 -0800

    fixing test cases

--- King.Mapper.Generator.Unit/CodeGeneratorTests.cs
+++ King.Mapper.Generator.Unit/CodeGeneratorTests.cs
@@ -22,33 +22,30 @@
         }
 
         [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
         public void ConstructorLoaderNull()
         {
             var factory = Substitute.For<IRenderFactory>();
             var writer = Substitute.For<IFileWriter>();
 
-            new CodeGenerator(null, factory, writer);
+            Assert.Throws<ArgumentNullException>(() => new CodeGenerator(null, factory, writer));
         }
 
         [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
         public void ConstructorFactoryNull()
         {
             var loader = Substitute.For<ISchemaReader>();
             var writer = Substitute.For<IFileWriter>();
 
-            new CodeGenerator(loader, null, writer);
+            Assert.Throws<ArgumentNullException>(() => new CodeGenerator(loader, null, writer));
         }
 
         [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
         public void ConstructorWriterNull()
         {
             var loader = Substitute.For<ISchemaReader>();
             var factory = Substitute.For<IRenderFactory>();
 
-            new CodeGenerator(loader, factory, null);
+            Assert.Throws<ArgumentNullException>(() => new CodeGenerator(loader, factory, null));
         }
 
         [Test]
